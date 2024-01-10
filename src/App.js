449d7ba2bcc132ec55ea7183457c57e58b797bcd
Shoxjaxon Zayniddinov <shoxjaxonzayniddinov016@gmail.com>
2024-01-10 14:34:43 +0500

    Cart Js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Cart from "./components/cart/Cart";
 
 function App() {
   return (
-    <div>
-      <h1>App.js</h1>
+    <div className="App">
+      <Cart/>
     </div>
   );
 }
