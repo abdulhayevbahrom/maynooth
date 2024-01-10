804095e4ca7c9345907beec932d419f6b44e994e
Xbro1305 <x.bro2010@gmail.com>
2024-01-10 14:19:29 +0500

    Added catalog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Catalog } from "./Components/Catalog/Catalog";
 
 function App() {
   return (
-    <div>
-      <h1>App.js</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Catalog/mapData.js
@@ -0,0 +1,70 @@
+export const fylterData = {
+  Price: ["150-250", "250-350", "350-550", "550-850", "850-1000", "1000-2000"],
+  Size: ["2 seaters", "3 seaters", "4 seaters", "Corner", "Sofabels"],
+};
+
+export const products = [
+  {
+    title: "Comfort",
+    description: "Great sofa",
+    size: "2 seaters",
+    price: 199,
+    images: [
+      "https://hmifurniture.in/wp-content/uploads/2021/12/WhatsApp-Image-2021-12-20-at-10.01.50-PM-e1640681833617.jpeg",
+    ],
+  },
+  {
+    title: "Comfort",
+    description: "Great sofa",
+    size: "Sofabels",
+    price: 599,
+    images: [
+      "https://hmifurniture.in/wp-content/uploads/2021/12/WhatsApp-Image-2021-12-20-at-10.01.50-PM-e1640681833617.jpeg",
+    ],
+  },
+  {
+    title: "Comfort",
+    description: "Great sofa",
+    size: "4 seaters",
+    price: 499,
+    images: [
+      "https://hmifurniture.in/wp-content/uploads/2021/12/WhatsApp-Image-2021-12-20-at-10.01.50-PM-e1640681833617.jpeg",
+    ],
+  },
+  {
+    title: "Comfort",
+    description: "Great sofa",
+    size: "Corner",
+    price: 799,
+    images: [
+      "https://hmifurniture.in/wp-content/uploads/2021/12/WhatsApp-Image-2021-12-20-at-10.01.50-PM-e1640681833617.jpeg",
+    ],
+  },
+  {
+    title: "Comfort",
+    description: "Great sofa",
+    size: "3 seaters",
+    price: 1099,
+    images: [
+      "https://hmifurniture.in/wp-content/uploads/2021/12/WhatsApp-Image-2021-12-20-at-10.01.50-PM-e1640681833617.jpeg",
+    ],
+  },
+  {
+    title: "Comfort",
+    description: "Great sofa",
+    size: "2 seaters",
+    price: 899,
+    images: [
+      "https://hmifurniture.in/wp-content/uploads/2021/12/WhatsApp-Image-2021-12-20-at-10.01.50-PM-e1640681833617.jpeg",
+    ],
+  },
+  {
+    title: "Comfort",
+    description: "Great sofa",
+    size: "Corner",
+    price: 299,
+    images: [
+      "https://hmifurniture.in/wp-content/uploads/2021/12/WhatsApp-Image-2021-12-20-at-10.01.50-PM-e1640681833617.jpeg",
+    ],
+  },
+];
